3c131ea4ff042c0e7bfe1dc405bd7b0a66bdc94b
EstefaniaAlegriavazquez <alegriaestefania934@gmail.com>
2025-01-31 20:22:57 -0600

    Bucle 4 Triangulo de números
    En el código el usuario ingrese un número y haga un tringulo rectangulo con los números menores

--- untitled/src/Bucle4.java
+++ untitled/src/Bucle4.java
@@ -8,8 +8,8 @@ public class Bucle4 {
         int num = scanner.nextInt();
 
         for (int i = 1; i <= num; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
             }
             System.out.println();
         }
