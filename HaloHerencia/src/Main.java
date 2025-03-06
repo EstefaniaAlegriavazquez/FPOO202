894108099ee11bcbf3b0ec14cb1e4569048816f4
EstefaniaAlegriavazquez <alegriaestefania934@gmail.com>
2025-03-06 14:48:00 -0600

    Practica 14 terminada
    Uso de Herencia y Polimorfismo

--- HaloHerencia/src/Main.java
+++ HaloHerencia/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpartanII jefemaestro= new SpartanII("Jonh",90,"rifle de asalto",0);
+        jefemaestro.mostrarInfo();
+        jefemaestro.usarManejoAvanzado();
+        jefemaestro.atacar("Convenant");
+        jefemaestro.recibirDano(50);
+        jefemaestro.recargarEscudo();
+
+
+        SpartanIII nobleSix = new SpartanIII("Emile",100,"Agujoneador",0);
+        nobleSix.mostrarInfo();
+        nobleSix.camuflajeActivo();
+        nobleSix.atacar("Grunt");
+
+
+        SpartanIV Locke= new SpartanIV("Locke",70,"DMR",0);
+
+
+
     }
 }
